f07c30768ddbbd918d102e1a125daae364723f6d
oni <rainbow2774@naver.com>
2022-04-21 02:51:07 +0900

    chp8 복습 및 주석 추가

--- ch08/src/Ex8_6.java
+++ ch08/src/Ex8_6.java
@@ -1,14 +1,14 @@
 class Ex8_6 {
 	public static void main(String args[]) {
 		try {
-			Exception e = new Exception("°íÀÇ·Î ¹ß»ı½ÃÄ×À½.");
-			throw e;	 // ¿¹¿Ü¸¦ ¹ß»ı½ÃÅ´
-		//  throw new Exception("°íÀÇ·Î ¹ß»ı½ÃÄ×À½.");
+			Exception e = new Exception("ê³ ì˜ë¡œ ë°œìƒì‹œì¼°ìŒ."); //ë§¤ê°œë³€ìˆ˜ë¡œ ë“¤ì–´ê°€ëŠ” Stringì´ ì˜¤ë¥˜ë©”ì‹œì§€ê°€ ëœë‹¤!
+			throw e;	 // ì˜ˆì™¸ë¥¼ ë°œìƒì‹œí‚´
+		//  throw new Exception("ê³ ì˜ë¡œ ë°œìƒì‹œì¼°ìŒ."); ìœ„ì˜ ë‘ì¤„ì„ ì´ë ‡ê²Œ í•œì¤„ë¡œ ìš”ì•½ê°€ëŠ¥!
 
 		} catch (Exception e)	{
-			System.out.println("¿¡·¯ ¸Ş½ÃÁö : " + e.getMessage());
+			System.out.println("ì—ëŸ¬ ë©”ì‹œì§€ : " + e.getMessage());
 			e.printStackTrace();
 		}
-		System.out.println("ÇÁ·Î±×·¥ÀÌ Á¤»ó Á¾·áµÇ¾úÀ½.");
+		System.out.println("í”„ë¡œê·¸ë¨ì´ ì •ìƒ ì¢…ë£Œë˜ì—ˆìŒ.");
 	}
 }
--- ch08/src/ExceptionTest.java
+++ ch08/src/ExceptionTest.java
@@ -2,7 +2,7 @@
 public class ExceptionTest {
 
 	public static void main(String[] args) {
-		System.out.println(args[0]); //컴파일 에러
+		System.out.println(args[0]); //
 		
 	}
 
