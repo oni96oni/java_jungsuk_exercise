55da40676d7199703d4fdd30fe0aba9ce98329bf
oni <rainbow2774@naver.com>
2022-07-21 13:38:45 +0900

    예시코드 추가 및 복습

--- ch02/src/Ex2_11.java
+++ ch02/src/Ex2_11.java
@@ -2,15 +2,20 @@ class Ex2_11 {
 	public static void main(String[] args) {
 		short sMin = -32768,  sMax = 32767;
 		char  cMin = 0,       cMax = 65535;
-
+		char test = 'a';
 		System.out.println("sMin  = " + sMin);
 		System.out.println("sMin-1= " + (short)(sMin-1));
 		System.out.println("sMax  = " + sMax);
 		System.out.println("sMax+1= " + (short)(sMax+1));
 		System.out.println("cMin  = " + (int)cMin);
-		//System.out.println("cMin-1= " + (int)--cMin);
+//		System.out.println("cMin-1= " + (int)--cMin);
 		System.out.println("cMin-1= " + (int)(cMin-1));
 		System.out.println("cMax  = " + (int)cMax);
 		System.out.println("cMax+1= " + (int)++cMax);
+		System.out.println("cMin  = " + (char)cMin);
+//		System.out.println("cMin-1= " + (char)--cMin);
+		System.out.println("cMin-1= " + (char)(cMin-1));
+		System.out.println("cMax  = " + (char)cMax);
+		System.out.println("cMax+1= " + (char)++cMax);
 	}
 }
